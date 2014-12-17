92b30bccd2201bb73385ed62044ec305f192bfbf
Vitalij <v.chubatyj@ukr.net>
2014-12-17 12:25:52 +0200

    Changes from Dec 17 2014
    Sync with Egnlish and Russian

--- locale/Ukrainian/global.php
+++ locale/Ukrainian/global.php
@@ -32,4 +32,86 @@
 //Safe Redirect$locale['global_500'] = "Ваш запит перенаправлено на %s, будь-ласка, зачекайте. Якщо перенаправлення не відбулось, натисніть тут";
 // Captcha Locales$locale['global_600'] = "Код підтвердження";$locale['recaptcha'] = "en";
 //Miscellaneous$locale['global_900'] = "Неможливо конвертувати HEX в DEC";//Language Selection$locale['global_ML100'] = "Мова:";$locale['global_ML101'] = "- виберіть мову -";$locale['global_ML102'] = "Мова сайту";$locale['flood'] = "Нові публікації заблоковані до завершення встановленого часу контролю за флудом. Будь ласка, зачекайте %s";$locale['no_image'] = "Зображення відсутнє";
-$locale['send_message'] = 'Надіслати повідомлення';$locale['go_profile'] = 'Перейти у профіль %s';?>
+// ex. oneword.locale.php
+$locale['send_message'] = 'Надіслати повідомлення';$locale['go_profile'] = 'Перейти у профіль %s';
+// Greetings
+$locale['hello'] = "Вітаємо!";
+$locale['goodbye'] = "На все добре!";
+$locale['welcome'] = "До наступної зустрічі";
+$locale['home'] = 'Головна';
+
+// Status
+$locale['error'] = "Помилка!";
+$locale['success'] = "Успіх!";
+$locale['enable'] = "Дозволити";
+$locale['disable'] = "Заборонити";
+$locale['no'] = "Ні";
+$locale['yes'] = "Так";
+$locale['off'] = "Вимкнути";
+$locale['on'] = "Ввімкнути";
+$locale['or'] = 'або';
+$locale['by'] = 'за';
+$locale['in'] = 'в';
+$locale['of'] = 'з';
+
+// Navigation
+$locale['next'] = "Настпуний";
+$locale['pevious'] = "Попередній";
+$locale['back'] = "Назад";
+$locale['forward'] = "Далі";
+$locale['go'] = 'Перейти';
+$locale['cancel'] = 'Скасувати';
+
+// Action
+$locale['add'] = "Додати";
+$locale['save'] = "Зберегти";
+$locale['update'] = "Оновити";
+$locale['updated'] = 'Оновлено';
+$locale['remove'] = "Видалити";
+$locale['delete'] = 'Видалити';
+$locale['search'] = "Знайти";
+$locale['help'] = "Допомога";
+$locale['register'] = "Реєстрація";
+$locale['ban'] = "Бан";
+$locale['reactivate'] = "Відновлення";
+$locale['user'] = "Користувач";
+$locale['promote'] = "Поширення";
+$locale['show'] = 'Показати';
+
+//Tables
+$locale['status'] = "Стан";
+$locale['order'] = "Порядок";
+$locale['sort'] = 'Сортування';
+$locale['id'] = "ID";
+$locale['title'] = "Назва";
+$locale['rights'] = "Права";
+$locale['image'] = "Зображення";
+$locale['info'] = "Додатково";
+
+// Forms
+$locale['choose'] = "Будь ласка, виберіть...";
+$locale['root'] = 'Як кореневий';
+$locale['choose-user'] = 'Будь ласка, виберіть користувача...';
+$locale['parent'] = "Створити як кореневий..";
+$locale['order'] = "Порядок розташування";
+$locale['status'] = "Стан";
+$locale['note'] = "Відмітити";
+$locale['publish'] = "Опубліковано";
+$locale['unpublish'] = "Не опубліковано";
+$locale['draft'] = "Чернетка";
+$locale['settings'] = "Налаштування";
+$locale['posted'] = "опубліковано";
+$locale['in'] = "в";
+$locale['profile'] = "Профіль";
+$locale['edit'] = "Редагування";
+$locale['view'] = "Перегляд";
+$locale['login'] = "Авторизація";
+$locale['logout'] = "Вихід";
+$locale['admin-logout'] = "Вихід адміністратора";
+$locale['message'] = "Приватні повідомлення";
+$locale['logged'] = "Авторизовано як ";
+$locale['version'] = "Версія ";
+$locale['browse'] = "Перегляд ...";
+$locale['close'] = 'Закрити';
+$locale['nopreview'] = 'Дані для перегляду відсутні';
+?
